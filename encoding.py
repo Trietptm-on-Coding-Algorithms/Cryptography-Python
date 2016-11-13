8c92ac33c15444f586e8fe56d338b68fc88449e1
ashutosh1206 <ashutosh.ahelleya@gmail.com>
2016-11-13 15:19:14 +0530

    Fixed XOR

--- encoding.py
+++ encoding.py
@@ -1,27 +1,9 @@
-num1 = raw_input("Enter the first number in hex form: ")
-c1 = int(num1,16)
-binum1 = bin(c1)
-print "binum1: ",binum1
-
-num2 = raw_input("Enter the second number in hex form: ")
-c2 = int(num2,16)
-binum2 = bin(c2)
-print "binum2: ",binum2
-
-str1 = str(binum1).strip('0b')
-str2 = str(binum2).strip('0b')
-str3 = " "
-print "str1: ", str1
-print "str2: ", str2
-
-for i in range(len(str1)):
-    if str1[i] == str2[i]:
-        str3 += '0'
-    else:
-        str3 += '1'
-i = int(str3)
-print hex(i)
-
-
-
+temp1 = raw_input("Enter a number in hex: ")
+num1 = int(temp1,16)
+print "num1: ",num1
+temp2 = raw_input("Enter another number in hex: ")
+num2 = int(temp2,16)
+print "num2: ",num2
+xored = hex(num1 ^ num2)
+print xored
 
