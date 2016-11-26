abf8649a22fbd6638499c8146553a5bfa3212747
ashutosh1206 <ashutosh.ahelleya@gmail.com>
2016-11-26 15:37:58 +0530

    Frequency Analysis of a String

--- freqanalysis.py
+++ freqanalysis.py
@@ -1,1 +1,16 @@
 str1 = raw_input("Enter a string to be analysed for frequency analysis: ")
+list1 = list(str1)
+list2 = []
+list3 = []
+for i in list1:
+    if i not in list2:
+        list2.append(i)
+        list3.append(1)
+    else:
+        for j in range(len(list2)):
+            if i == list1[j]:
+                list3[j] += 1
+for i in range(len(list2)):
+    print "Frequency of the alphabet", list2[i], "is", list3[i]
+
+
