380b2765aa596c168e37a4c3a4a2a1a999aacedd
ashutosh1206 <ashutosh.ahelleya@gmail.com>
2017-03-05 14:48:38 +0530

    Added Brute Forcing

--- Single Byte XOR.py
+++ Single Byte XOR.py
@@ -5,14 +5,20 @@ for i in xrange(len(str1)-1):
         str1[i] = str1[i] + " "
 str1 = ''.join(str1)
 str1 = str1.split(" ")
+temp = str1
 
-
-for i in xrange(len(str1)):
-    str1[i] = hex(int(str1[i],16) ^ ord('X'))
-    str1[i] = str1[i].replace("0x","")
-str1 = ''.join(str1)
-str1 = str1.decode("hex")
-
-print str1
+for j in xrange(1,257):
+    fl = 0
+    str1 = temp
+    for i in xrange(len(str1)):
+        str1[i] = hex(int(str1[i],16) ^ j)
+        str1[i] = str1[i].replace("0x","")
+    str1 = ''.join(str1)
+    if(len(str1)%2!=0):
+        str1 = "0"+str1
+    str1 = str1.decode("hex")
+    print str1
+    
+            
 
 
