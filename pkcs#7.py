8ca3060cf967b0f8940d36e0beaa9d33844bbb35
ashutosh1206 <ashutosh.ahelleya@gmail.com>
2017-03-06 21:02:45 +0530

    Fixed: Number of bytes to be padded

--- pkcs#7.py
+++ pkcs#7.py
@@ -1,17 +1,33 @@
 n = input("Enter the size of the block required: ")
 str1 = raw_input("Enter the string to be padded: ")
-
+print "The encoded form of the original string without padding: ",str1.encode("hex")
 l = len(str1)
-padlen = n % l
+print "Length of the string given as input: ",l
 
-padbyte = hex(padlen).strip('0x')
+if n>l:
+    padlen = n % l
+else:
+    i=2;
+    while i<50000:
+        temp = n
+        temp *= i
+        if temp > l:
+            padlen = temp % l
+            break
+        i += 1
+
+padbyte = hex(padlen).replace("0x","")
+print "The byte to be padded at the end of the string: ",padbyte
+
+print "padbyte: ",padbyte
 if(len(padbyte)%2!=0):
     padbyte = "0" + padbyte
 
 hexstr = str1.encode("hex")
 hexstr += padlen*padbyte
-
+print "The hex form of the string is: ", hexstr
 hexstr = hexstr.decode("hex")
-print hexstr
+print "The hex decoded, padded hex string: ",hexstr
+
 
 
