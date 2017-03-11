32e55a3a788bf5620fd42880a1db4dbf22c90f48
ashutosh1206 <ashutosh.ahelleya@gmail.com>
2017-03-11 17:37:00 +0530

    Fixed Issues

--- freqanalysis.py
+++ freqanalysis.py
@@ -1,4 +1,5 @@
 str1 = raw_input("Enter a string to be analysed for frequency analysis: ")
+str1 = str1.replace(' ','')
 list1 = list(str1)
 list2 = []
 list3 = []
@@ -8,7 +9,7 @@ for i in list1:
         list3.append(1)
     else:
         for j in range(len(list2)):
-            if i == list1[j]:
+            if i == list1[j] and i!=j:
                 list3[j] += 1
 for i in range(len(list2)):
     print "Frequency of the alphabet", list2[i], "is", list3[i]
