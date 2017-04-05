6b6580d1a7d500ab227c766aa37bd3188181df5a
ashutosh1206 <ashutosh.ahelleya@gmail.com>
2017-04-05 09:24:36 +0530

    Improved Efficiency

--- Caesar Cipher.py
+++ Caesar Cipher.py
@@ -1,16 +1,29 @@
-encstr = []
-decstr = raw_input("Enter the string to be encoded: ")
-key = input("Enter the length upto which you want to shift the alphabets: ")
-decstr = list(decstr)
-for i in decstr:
-    if (ord(i)+key)>122:
-        newkey = key - (122 - ord(i))
-        i = chr(97 + (newkey-1))
-        encstr.append(i)
-    else:
-        i = chr(ord(i)+key)
-        encstr.append(i)
-encstr = ''.join(encstr)
-print "The encoded form of the string is: ",encstr
+import sys
 
-    
+s = raw_input().strip()
+k = int(raw_input().strip())  
+k=k%26
+encstr = []
+decstr = list(s)
+for i in decstr:
+    if ord(i)>=97 and ord(i)<=122:
+        if (ord(i)+k)>122:
+            newkey = k - (122 - ord(i))
+            i = chr(97 + (newkey-1))
+            encstr.append(i)
+        else:
+            i = chr(ord(i)+k)
+            encstr.append(i)
+    elif ord(i)>=65 and ord(i)<=90:
+        if (ord(i)+k)>90:
+            newkey = k - (90 - ord(i))
+            i = chr(65 + (newkey-1))
+            encstr.append(i)
+        else:
+            i = chr(ord(i)+k)
+            encstr.append(i)
+    else:
+        encstr.append(i)
+        
+encstr = ''.join(encstr)
+print encstr
